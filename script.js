6a5aeede29da6e96795108d81440b4cf7824799b
fabuandras <fabuandras@gmail.com>
2025-02-26 10:08:31 +0100

    JS alapok - gyakorló feladat - [ FELKESZ ]

--- script.js
+++ script.js
@@ -5,9 +5,69 @@ window.addEventListener("load", function () {
 });
 
 function elemekElerese1() {
-   
+    const ELEM=document.querySelectorAll("section h2")[0]
+    let tartalom = ELEM.innerHTML
+    console.log(tartalom)
 }
+
 function elemekElerese2() {
-   
+    const ELEM = document.getElementById("ide");
+    ELEM.innerHTML +=
+            `
+            <p>Jó reggelt!</p>
+            `;
+    console.log(ELEM);
+}
+
+function elemekElerese3() {
+    const ELEM = document.querySelectorAll(".ide")[0];
+    ELEM.innerHTML += `
+        <p>Jó reggelt!</p>
+    `;
+    console.log(ELEM);
+}
+
+function elemekElerese4() {
+    const ELEM = document.getElementById("lista");
+    let txt = "<ul>";
     
+    for (i = 0; i < 5; i++) {
+        let randomSzam = Math.floor(Math.random()*21 + 10);
+        txt += `<li>${randomSzam}</li>`;
+    }
+    
+    txt += "</ul>";
+    ELEM.innerHTML = txt;
+    console.log(ELEM);
+}
+
+function elemekFormazasa() {
+    const ELEM = document.getElementById("lista");
+    ELEM.classList.add("formazott");
+}
+
+/* -------------------------------------------------- */
+
+function esemenyKezelesPelda() {
+    let elem4 = document.querySelectorAll(".lista")[0];
+    elem4.addEventListener("click", fvenynev);
+}
+
+function fvenynev() {
+    let elem4 = document.querySelectorAll(".lista")[0];
+    const kattintasUtanElem1 = document.querySelectorAll(".kattintasutan1")[0];
+    kattintasUtanElem1.appendChildÍ(elem4);
+}
+
+/* -------------------------------------------------- */
+
+function esemenyKezeles1() {
+    const listaElem = document.getElementById("lista");
+    const kattintasUtanElem = document.getElementById("kattintasutan");
+    
+    listaElem.addEventListener("click", fveny);
+    
+        function fveny() {
+        kattintasUtanElem.innerHTML = listaElem.innerHTML;
+    };
 }
